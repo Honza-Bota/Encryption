5920836f3f02a6266a24a47ae03d0fc6562f4a79
Boťa Jan <botajan@email.cz>
2021-03-13 13:30:49 +0100

    Dokončení?

---
+++ Crypto.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+
+namespace Encryption
+{
+    partial class Program
+    {
+        class Crypto
+        {
+            public string XOR(string zprava, string klic)
+            {
+                string zasifrovana = "";
+                zprava = NormalizedString(zprava);
+                klic = NormalizedString(klic);
+
+                for (int i = 0, j = 0; i < zprava.Length; i++, j++)
+                {
+                    if (j >= klic.Length) j = 0;
+
+                    zasifrovana += (char)(zprava[i] ^ klic[j]);
+                }
+
+                return zasifrovana;
+            }
+
+            public string Cesar(string zprava, int posun)
+            {
+                string zasifrovana = ""; 
+                zprava = NormalizedString(zprava);
+
+                for (int i = 0; i < zprava.Length; i++)
+                {
+                    zasifrovana += (char)((int)zprava[i] + posun);
+                }
+
+                return zasifrovana;
+            }
+
+            public string SaveText(string text, string soubor)
+            {
+                if (soubor.Contains(".txt"))
+                {
+                    File.WriteAllText(soubor, text);
+                    return "Soubor úspěšně uložen.";
+                }
+
+                return "Nastala chyba při ukládání!";
+            }
+
+            private string NormalizedString(string zprava)
+            {
+                string edited;
+
+                edited = zprava.Replace(" ", "");
+                edited = edited.Replace('Á', 'a');
+                edited = edited.Replace('á', 'a');
+                edited = edited.Replace('Č', 'c');
+                edited = edited.Replace('č', 'c');
+                edited = edited.Replace('Ď', 'd');
+                edited = edited.Replace('ď', 'd');
+                edited = edited.Replace('É', 'e');
+                edited = edited.Replace('é', 'e');
+                edited = edited.Replace('Ě', 'e');
+                edited = edited.Replace('ě', 'e');
+                edited = edited.Replace('Í', 'i');
+                edited = edited.Replace('í', 'i');
+                edited = edited.Replace('Ň', 'n');
+                edited = edited.Replace('ň', 'n');
+                edited = edited.Replace('Ó', 'o');
+                edited = edited.Replace('ó', 'o');
+                edited = edited.Replace('Ř', 'r');
+                edited = edited.Replace('ř', 'r');
+                edited = edited.Replace('Š', 's');
+                edited = edited.Replace('š', 's');
+                edited = edited.Replace('Ť', 't');
+                edited = edited.Replace('ť', 't');
+                edited = edited.Replace('Ú', 'u');
+                edited = edited.Replace('ú', 'u');
+                edited = edited.Replace('Ů', 'u');
+                edited = edited.Replace('ů', 'u');
+                edited = edited.Replace('Ý', 'y');
+                edited = edited.Replace('ý', 'y');
+                edited = edited.Replace('Ž', 'z');
+                edited = edited.Replace('ž', 'z');
+                                             
+                edited = edited.ToUpper();
+
+                return edited;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Encryption
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             ConsoleKeyInfo pokracovani;
-            Crypto cry = new Crypto();
+            Crypto crypto = new Crypto();
 
             do
             {
@@ -24,41 +25,122 @@ namespace Encryption
                 Console.WriteLine("------------------------");
                 Console.WriteLine("1) Bloková šifra XOR");
                 Console.WriteLine("2) Cesarova šifra");
+                Console.WriteLine("3) Bloková šifra XOR - dešifrování");
+                Console.WriteLine("4) Cesarova šifra - dešifrování");
                 Console.WriteLine("------------------------");
                 Console.WriteLine();
-                Console.Write("Zadej číslo šifry <1, 2>: ");
+                Console.Write("Zadej číslo šifry <1, 4>: ");
 
                 string sifra = Console.ReadLine();
-                if (sifra == "1") cry.XOR();
-                else if (sifra == "2") cry.Cesar();
-                else Console.WriteLine("\n --- Neplatný vstup! --- ");
+                Console.WriteLine("------------------------");
 
-                Console.WriteLine();
+                if (sifra == "1")
+                {
+                    Console.Write("Zadej zprávu pro zašifrování: ");
+                    string zprava = Console.ReadLine();
+                    Console.Write("Zadej klíč pro šifrování: ");
+                    string klic = Console.ReadLine();
+
+                    string zasifrovana = crypto.XOR(zprava, klic);
+
+                    Console.WriteLine();
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine($"Zašifrovaná zpráva:");
+                    Console.WriteLine(zasifrovana);
+                    Console.WriteLine("------------------------");
+                    Console.Write("Chcete text uložit do souboru? Pokud ne, stiskňete Enter. Pokud ano, zadejte jméno souboru pro uložení: ");
+                    string ulozeni = Console.ReadLine();
+                    if (ulozeni != "") crypto.SaveText(zasifrovana, ulozeni);
+
+                }
+                else if (sifra == "2")
+                {
+                    Console.Write("Zadej zprávu pro zašifrování: ");
+                    string zprava = Console.ReadLine();
+                    Console.Write("Zadej posun <1, 25>: ");
+                    string _posun = Console.ReadLine();
+                    int posun;
+                    if (int.TryParse(_posun, out posun) && posun <= 25 && posun > 0)
+                    {
+                        string zasifrovana = crypto.Cesar(zprava, posun);
+
+                        Console.WriteLine();
+                        Console.WriteLine("------------------------");
+                        Console.WriteLine($"Zašifrovaná zpráva:");
+                        Console.WriteLine(zasifrovana);
+                        Console.WriteLine("------------------------");
+                        Console.Write("Chcete text uložit do souboru? Pokud ne, stiskňete Enter. Pokud ano, zadejte jméno souboru pro uložení: ");
+                        string ulozeni = Console.ReadLine();
+                        if (ulozeni != "") crypto.SaveText(zasifrovana, ulozeni);
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("---- Neplatný vstup pro posun!! ----");
+                    }
+                }
+                else if (sifra == "3")
+                {
+                    Console.Write("Zadej zprávu pro dešifrování: ");
+                    string zprava = Console.ReadLine();
+                    Console.Write("Zadej klíč pro dešifrování: ");
+                    string klic = Console.ReadLine();
+
+                    string desfirovana = crypto.XOR(zprava, klic);
+
+                    Console.WriteLine();
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine($"Dešifrovaná zpráva:");
+                    Console.WriteLine(desfirovana);
+                    Console.WriteLine("------------------------");
+                    Console.Write("Chcete text uložit do souboru? Pokud ne, stiskňete Enter. Pokud ano, zadejte jméno souboru pro uložení: ");
+                    string ulozeni = Console.ReadLine();
+                    if (ulozeni != "") crypto.SaveText(desfirovana, ulozeni);
+                }
+                else if (sifra == "4")
+                {
+                    Console.Write("Zadej zprávu pro dešifrování: ");
+                    string zprava = Console.ReadLine();
+                    Console.Write("Zadej posun <1, 25>: ");
+                    string _posun = Console.ReadLine();
+                    int posun;
+                    if (int.TryParse(_posun, out posun) && posun <= 25 && posun > 0)
+                    {
+                        string desifrovana = crypto.Cesar(zprava, posun * (-1));
+
+                        Console.WriteLine();
+                        Console.WriteLine("------------------------");
+                        Console.WriteLine($"Dešifrovaná zpráva:");
+                        Console.WriteLine(desifrovana);
+                        Console.WriteLine("------------------------");
+                        Console.Write("Chcete text uložit do souboru? Pokud ne, stiskňete Enter. Pokud ano, zadejte jméno souboru pro uložení: ");
+                        string ulozeni = Console.ReadLine();
+                        if (ulozeni != "") crypto.SaveText(desifrovana, ulozeni);
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("---- Neplatný vstup pro posun!! ----");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(" --- Neplatný vstup! --- ");
+                    Console.WriteLine();
+                }
+
                 Console.WriteLine("-------------------------------------");
-                Console.Write("Přejete si pokračovat? <Y:N>: ");
+                Console.Write("Pokud si přejete pokračovat stiskněte 'Y': ");
                 pokracovani = Console.ReadKey();
+                Console.WriteLine();
 
             } while (pokracovani.Key == ConsoleKey.Y);
 
 
-
-            Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("----------- Pro ukončení stiskni Enter -----------");
             Console.ReadLine();
-        }
-
-        class Crypto
-        {
-            public void XOR()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Cesar()
-            {
-                throw new NotImplementedException();
-            }
         }
     }
 }
